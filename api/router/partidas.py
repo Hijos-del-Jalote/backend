9334544960fe5ce0532a93ec280aa5e4c264df2e
Ignacio <iscavuzzo@mi.unc.edu.ar>
2023-09-24 01:38:00 -0300

    endpoint crear_partida

--- api/router/partidas.py
+++ api/router/partidas.py
@@ -1,6 +1,32 @@
-from fastapi import APIRouter
+from typing import Optional
+from fastapi import APIRouter, HTTPException, status
 from pony.orm import db_session
-from db.models import Partida
+from db.models import Partida, db
+from pydantic import BaseModel
 
 partidas_router = APIRouter()
+
+class PartidaIn(BaseModel):
+    nombrePartida: str
+
+class PartidaOut(BaseModel):
+    idPartida: int
+
+@partidas_router.post("",
+                     response_model=PartidaOut, 
+                     status_code=status.HTTP_201_CREATED)
+async def crear_partida(partida: PartidaIn) -> PartidaOut:
+    with db_session:
+
+        if(len(partida.nombrePartida) == 0):
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                                detail="Nombre vacio")
+        else:
+            nueva_partida = Partida(nombre = partida.nombrePartida,
+                                    maxJug = 12,
+                                    minJug = 4)
+            db.commit()
+        return PartidaOut(idPartida = nueva_partida.id) # se lo asigna pony solo
+        
+
  
