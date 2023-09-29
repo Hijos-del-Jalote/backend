ba3e55237672dc28b68f9836f3fa880acddcb72d
Ignacio <iscavuzzo@mi.unc.edu.ar>
2023-09-29 14:41:25 -0300

    finalizar partida y text

--- api/router/schemas.py
+++ api/router/schemas.py
@@ -2,3 +2,13 @@ from pydantic import BaseModel
 
 class PlayerResponse(BaseModel):
     id: int
+
+class PartidaIn(BaseModel):
+    nombrePartida: str
+
+class PartidaOut(BaseModel):
+    idPartida: int
+
+class EstadoPartida(BaseModel):
+    finalizada: bool
+    idGanador: int
--- tests/test_finalizar_partida.py
+++ tests/test_finalizar_partida.py
@@ -10,10 +10,8 @@ from tests.test_newplayer import random_user
 
 client = TestClient(app)
 
-
-def test_iniciar_partida():
-    # parte parecida a test_newplayer pero necesito el idHost
-
+def test_finalizar_partida():
+    
     jugadores = []
     for i in range(4):
         username = random_user()
@@ -23,10 +21,6 @@ def test_iniciar_partida():
     
     partida = random_user()
     host = jugadores[0]
-    
-    # partida no existente
-    response = client.put(f"partidas/iniciar?idPartida=1234")
-    assert response.status_code == status.HTTP_404_NOT_FOUND
 
     # creo partida
     response = client.post(f'partidas/?nombrePartida={partida}&idHost={host.id}') 
@@ -34,23 +28,43 @@ def test_iniciar_partida():
     with db_session:
         partida = Partida.get(id = response.json()["idPartida"])
 
-    # cantidad incorrecta de jugadores
-    response = client.put(f"partidas/iniciar?idPartida={partida.id}")
-    assert response.status_code == status.HTTP_400_BAD_REQUEST
-    
     # uno jugadores a partida
     for i in range(1,4):
         client.post(f"partidas/unir?idPartida={partida.id}&idJugador={jugadores[i].id}")
     
     # partida correcta
     response = client.put(f"partidas/iniciar?idPartida={partida.id}")
-    assert response.status_code == status.HTTP_200_OK
-    
-    # verifico que se haya iniciado
+
     with db_session:
         partida = Partida.get(id = partida.id)
-    assert partida.iniciada == True
+        
+        # hago una simulacion de juego
+        for jugador in partida.jugadores:
+            jugador.isAlive = False
+            ultimojugador = jugador
+        
+        ultimojugador.isAlive = True
 
-    # partida ya iniciada
-    response = client.put(f"partidas/iniciar?idPartida={partida.id}")
-    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    # partida finalizada
+    response = client.get(f"partidas/estado?idPartida={partida.id}")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["finalizada"] == True
+    assert response.json()["idGanador"] == ultimojugador.id
+
+    # partida no finalizada
+    with db_session:
+        partida = Partida.get(id = partida.id)
+        
+        # hago una simulacion de juego
+        for jugador in partida.jugadores:
+            jugador.isAlive = True
+
+    response = client.get(f"partidas/estado?idPartida={partida.id}")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["finalizada"] == False
+
+    # partida no existente
+    response = client.get(f"partidas/estado?idPartida=1234")
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    
