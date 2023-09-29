53e447d0ef61d835c150bae05875175d2e3e740f
FranMartiarena <franciscomartiarena@gmail.com>
2023-09-29 19:53:53 -0300

    primeros cambios

--- api/router/cartas.py
+++ api/router/cartas.py
@@ -3,3 +3,12 @@ from pony.orm import db_session
 from db.models import *
 
 cartas_router = APIRouter()
+
+@cartas_router.post("/jugar", status_code=200)
+async def jugar_carta(id_carta:int):
+    carta = Carta.get(id=id_carta)
+    partida = carta.partida
+    with db_session:
+        if carta:
+            carta.jugador = None
+            
