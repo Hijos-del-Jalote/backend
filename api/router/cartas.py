5d96102d7cece6af15a849e93ad06be4ef5961bb
FranMartiarena <franciscomartiarena@gmail.com>
2023-10-01 11:32:24 -0300

    Terminado Efecto lanzallamas y test

--- api/router/cartas.py
+++ api/router/cartas.py
@@ -1,5 +1,27 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from pony.orm import db_session
 from db.models import *
 
 cartas_router = APIRouter()
+
+def efecto_lanzallamas(id_objetivo):
+    with db_session:
+        if (id_objetivo != None) & (Jugador.exists(id=id_objetivo)):
+            objetivo = Jugador[id_objetivo]
+            objetivo.isAlive = False
+            db.commit()
+        else:
+            raise HTTPException(status_code=400, detail="Jugador objetivo No existe o No proporcionado")
+            
+            
+@cartas_router.post("/jugar", status_code=200)
+async def jugar_carta(id_carta:int, id_objetivo:int | None = None):
+    with db_session:
+        carta = Carta.get(id=id_carta)
+        if carta and carta.jugador:
+            carta.jugador.cartas.remove(carta)
+            carta.descartada=True
+            if carta.template_carta.nombre == "Lanzallamas":
+                efecto_lanzallamas(id_objetivo)
+        else:
+            raise HTTPException(status_code=400, detail="Non existant id for carta")
