6ee651e9de780409ec4581339bdf3d5e0fc474ee
Ignacio <iscavuzzo@mi.unc.edu.ar>
2023-10-01 20:28:08 -0300

    agrego cartas a jugadores para test de obtener jugador

--- tests/populate_db.py
+++ tests/populate_db.py
@@ -1,7 +1,8 @@
 # Fake users and games for testing
 from pony.orm import db_session, count
-from db.models import Jugador,Partida
+from db.models import Jugador,Partida, Carta, TemplateCarta, Tipo_Carta
 from tests.test_newplayer import random_user
+import random
 
 def load_jugadores():
     #(id,nombre,isHost,partida)
@@ -37,12 +38,35 @@ def load_partidas():
                         iniciada=iniciada,
                         jugadores=jugadores)
 
+def load_cartas_jugadores():
 
+    template_lanzallamas = TemplateCarta(nombre="Lanzallamas",
+         descripcion="Esta es una carta de prueba", tipo=Tipo_Carta.accion)
+    template_comun = TemplateCarta(nombre="Carta Vacia", descripcion=
+         "Esta Carta No Hace Nada", tipo=Tipo_Carta.accion)
+
+    opciones_template = [template_comun, template_lanzallamas]
+
+    with db_session:
+        partida = Partida.get(id=1)
+        if count(Carta.select()) == 0:
+            id = 0
+            for jugador in partida.jugadores:
+                for i in range(4):
+                    template_random = random.choice(opciones_template)
+                    carta = Carta(id=id,
+                                  descartada=False,
+                                  template_carta=template_random,
+                                  partida=partida)
+                    jugador.cartas.add(carta)
+                    id += 1
+                
 
 @db_session
 def populate_db():
     load_jugadores()
     load_partidas()
+    load_cartas_jugadores()
 
 if __name__ == "__main__":
     populate_db()
