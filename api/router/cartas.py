20f02afaf40085138ceff5aa2d99f735c2063ca9
FranMartiarena <franciscomartiarena@gmail.com>
2023-10-01 21:01:35 -0300

    modularizacion de efectos

--- api/router/cartas.py
+++ api/router/cartas.py
@@ -1,17 +1,10 @@
 from fastapi import APIRouter, HTTPException
 from pony.orm import db_session
 from db.models import *
+from . import efectos_cartas
+
 
 cartas_router = APIRouter()
-
-def efecto_lanzallamas(id_objetivo):
-    with db_session:
-        if (id_objetivo != None) & (Jugador.exists(id=id_objetivo)):
-            objetivo = Jugador[id_objetivo]
-            objetivo.isAlive = False
-            db.commit()
-        else:
-            raise HTTPException(status_code=400, detail="Jugador objetivo No existe o No proporcionado")
             
             
 @cartas_router.post("/jugar", status_code=200)
@@ -23,6 +16,6 @@ async def jugar_carta(id_carta:int, id_objetivo:int | None = None):
             carta.jugador.cartas.remove(carta)
             carta.descartada=True
             if carta.template_carta.nombre == "Lanzallamas":
-                efecto_lanzallamas(id_objetivo)
+                efectos_cartas.efecto_lanzallamas(id_objetivo)
         else:
             raise HTTPException(status_code=400, detail="No existe el id de la carta ó jugador que la tenga")
