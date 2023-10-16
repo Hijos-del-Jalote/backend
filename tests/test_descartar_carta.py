c4f8c9119baa4d8da7f3bf085cdaf74edbbe37a5
juan pablo feltes <juan.pablo.feltes@mi.unc.edu.ar>
2023-10-16 17:47:08 -0300

    fixeado el test de descartar carta

--- tests/test_descartar_carta.py
+++ tests/test_descartar_carta.py
@@ -12,11 +12,10 @@ from api.router.cartas import *
 
 client = TestClient(app)
 
-crear_templates_cartas()
 
-
-def test_descartar_carta():
+def test_descartar_carta(cleanup_db_after_test):
     with db_session:
+        crear_templates_cartas()
         partida = Partida(nombre="partida", iniciada=True, finalizada=False , maxJug=5, minJug=4)
         commit()
         jugador = Jugador(nombre="Jugador", Posicion=0, Rol="Infectado", isAlive=True, partida=partida)
@@ -27,38 +26,47 @@ def test_descartar_carta():
         carta3 = Carta(template_carta="Lanzallamas", jugador=jugador, partida=partida)
         commit()
         assert len(jugador.cartas) == 4
-        try:
-            descartar_carta(carta1.id)  ## tratando de descartar la carta con 4 cartas en la mano
-        except HTTPException as e:      ## verifico que no se pueda descartar con 4 cartas en la mano
-            assert e.status_code == 400
-            assert e.detail == "No se puede descartar con 4 cartas en la mano"
+        
+        ## trato de descartar con 4 cartas
+        response= client.put(f'cartas/descartar_carta/{carta1.id}')
+        assert response.status_code == 400  ##verifico que no se descarte la carta con 4 cartas en la mano
         assert len(jugador.cartas) == 4
-        carta4 = Carta(template_carta="Lanzallamas", jugador=jugador, partida=partida)
+        assert carta1.descartada == False
+        
+        ## trato de descartar la carta infectado si solo tengo 1 carta infectado
         infectado1 = Carta(template_carta="Infectado", jugador=jugador, partida=partida)
         commit()
-        assert len(jugador.cartas) == 6 ##verifico que se creen las cartas
-        descartar_carta(carta1.id)
-        assert len(jugador.cartas) == 5  ##verifico que se descarte la carta
-        assert carta1.descartada == True 
-        # Caso carta La Cosa
-        try:
-            descartar_carta(lacosa.id)  ## tratando de descartar la carta La Cosa
-        except HTTPException as e:      ## verifico que no se pueda descartar
-            assert e.status_code == 400
-            assert e.detail == "No se puede descartar la carta La cosa"
+        assert len(jugador.cartas) == 5   
+        response= client.put(f'cartas/descartar_carta/{infectado1.id}') 
+        assert response.status_code == 400
+        assert len(jugador.cartas) == 5
+        assert infectado1.descartada == False  ## verifico que no se descarte la carta infectado
+        
+        #trato de descartar la cosa
+        response= client.put(f'cartas/descartar_carta/{lacosa.id}')
+        assert response.status_code == 400
         assert len(jugador.cartas) == 5
         assert lacosa.descartada == False
-        try :
-            descartar_carta(infectado1.id) ## tratando de descartar la carta Infectado
-        except HTTPException as e:        ## verifico que no se pueda descartar
-            assert e.status_code == 400
-            assert e.detail == "No se puede descartar la carta Infectado"
+        
+        #descarto una carta cualquiera
+    with db_session:                                   
+        response= client.put(f'cartas/descartar_carta/{carta1.id}')
+        assert response.status_code == 200
+        jugador = Jugador.get(id=jugador.id)
+        carta1 = Carta.get(id=carta1.id)
+        assert len(jugador.cartas) == 4  ##verifico que se descarte la carta
+        assert carta1.descartada == True 
+        
+        #descarto una carta infectado si tengo mas de 1
+    with db_session:                        
+        jugador = Jugador.get(id=jugador.id)
+        partida = Partida.get(id=partida.id)
         infectado2 = Carta(template_carta="Infectado", jugador=jugador, partida=partida)
         commit()
-        assert len(jugador.cartas) == 6
-        descartar_carta(infectado2.id)   ## descarto la carta Infectado una vez que hay mas de 1 carta de infectado
-        assert len(jugador.cartas) == 5
+        response= client.put(f'cartas/descartar_carta/{infectado2.id}')
+        assert response.status_code == 200
+    with db_session:                       
+        jugador = Jugador.get(id=jugador.id)
+        infectado2 = Carta.get(id=infectado2.id)
+        assert len(jugador.cartas) == 4  ##verifico que se descarte la carta
         assert infectado2.descartada == True
-
-
-
